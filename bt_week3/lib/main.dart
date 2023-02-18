f6e6555b0a30a3d28c45abb8b8a2f2c6ba576f6a
tanhao111 <haonguyentan2001@gmail.com>
2023-02-18 19:59:17 +0700

    Delete bt_week3 workflow

--- bt_week3/lib/main.dart
+++ bt_week3/lib/main.dart
@@ -11,7 +11,7 @@ class bt_week3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: welcomeScreen(),
     );
